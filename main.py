31ee1e281981c757b1809ada991b4481e5e42ec2
Salvador Guzman <guzmansalv@gmail.com>
2019-09-07 19:39:45 -0700

    finally can extract title

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
-import atexit
-import json
-import psycopg2
 from typing import Optional
+from typing import List
+import xml.etree.ElementTree as ET
+
+path: str = './notes.enex'
 
 
-def read_xml() -> json:
-    json_path: str = './people.json'
-    json_str: str = open(json_path, 'r').read()
+def read_xml() -> List[str]:
+    tree: ET = ET.parse(path)
+    root: ET.ElementTree = tree.getroot()
 
-    json_obj: json = json.loads(json_str)
-    return json_obj
+    items: List[str] = []
+    for child in root.getiterator():
+        print(child.tag, child.attrib)
+        if child.tag == 'title':
+            print(child.text.encode('utf8'))
 
-
-def con() -> psycopg2:
-    conn: psycopg2 = \
-        psycopg2.connect(user='admin', password='', host='127.0.0.1', port='5432', database='misc')
-
-    def clean_up() -> None:
-        conn.close()
-        print('Closing connection', conn)
-
-    atexit.register(clean_up)
-    return conn
+    return items
 
 
 def main() -> None:
-    print('Hello world')
+    read_xml()
 
 
 if __name__ == '__main__':
